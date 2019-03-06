08103c4c7674c63569e8c0d47fc594b3a4ab800e
Lorenz-Grohmann <lorenz.grohmann@gmx.de>
2019-03-06 23:34:06 +0100

    add OpenClose models

--- c_hack_api/api/api_models.py
+++ c_hack_api/api/api_models.py
@@ -7,5 +7,18 @@ from . import API
 from ..db_models import STD_STRING_SIZE
 
 ROOT_MODEL = API.model('RootModel', {
-
+    'openClose': fields.Url('api.openclose_period_list'),
 })
+
+OPEN_CLOSE_POST = API.model('OpenClosePOST', {
+    'begin': fields.Integer(title='Open From'),
+    'end': fields.Integer(title='Open Until'),
+    'comment': fields.String(max_length=STD_STRING_SIZE,title='Comment'),
+})
+
+OPEN_CLOSE_GET = API.inherit('OpenCloseGET', OPEN_CLOSE_POST, {
+    'id': fields.Integer(min=1, example=1, readonly=True, title='Internal Identifier'),
+})
+
+OPEN_CLOSE_PUT = API.inherit('OpenClosePUT', OPEN_CLOSE_POST, {})
+
