945b58a98ada6962d5af16a845e3e9a44abe2f2a
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-03-19 22:36:28 +0100

    Remove unnecessary stuff from config.

--- c_hack_api/config.py
+++ c_hack_api/config.py
@@ -8,35 +8,10 @@ class Config(object):
     DEBUG = False
     TESTING = False
     RESTPLUS_VALIDATE = True
-    JWT_CLAIMS_IN_REFRESH_TOKEN = True
-    JWT_SECRET_KEY = ''.join(hex(randint(0, 255))[2:] for i in range(16))
-    SQLALCHEMY_DATABASE_URI = 'sqlite://:memory:'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DB_UNIQUE_CONSTRAIN_FAIL = 'UNIQUE constraint failed'
     URI_BASE_PATH = '/'
-
-    LOGIN_PROVIDERS = ['Basic']
-
-    BASIC_AUTH_USERS = {}
-    BASIC_AUTH_ADMIN = []
-    BASIC_AUTH_KIOSK = []
-    BASIC_AUTH_USER = []
-
-    LDAP_URI = ""
-    LDAP_PORT = 0
-    LDAP_SSL = False
-    LDAP_START_TLS = False
-    LDAP_USER_SEARCH_BASE = ""
-    LDAP_GROUP_SEARCH_BASE = ""
-    LDAP_USER_RDN = ""
-    LDAP_USER_UID_FIELD = ""
-    LDAP_GROUP_MEMBERSHIP_FIELD = ""
-    LDAP_CONSUMER_FILTER = ""
-    LDAP_KIOSK_USER_FILTER = ""
-    LDAP_ADMIN_FILTER = ""
-    LDAP_CONSUMER_GROUP_FILTER = ""
-    LDAP_KIOSK_USER_GROUP_FILTER = ""
-    LDAP_ADMIN_GROUP_FILTER = ""
 
     LOGGING_CONFIGS = ['logging_config.json']
 
@@ -47,22 +22,9 @@ class Config(object):
 class ProductionConfig(Config):
     pass
 
-
 class DebugConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
-    JWT_SECRET_KEY = 'debug'
-
-    BASIC_AUTH_USERS = {
-        "admin": "admin",
-        "kiosk": "kiosk",
-        "user1": "user1",
-        "user2": "user2",
-    }
-
-    BASIC_AUTH_ADMIN = ["admin"]
-    BASIC_AUTH_KIOSK = ["kiosk"]
-    BASIC_AUTH_USER = ["user1", "user2"]
 
     LOGGING_CONFIGS = ['logging_config.json', 'logging_config_debug.json']
 
